817625fa0d5a04352ed327b65fe0818db8e63551
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-07-29 17:55:35 +0200

    Creating Java 17 hello world project.

---
+++ java-17-hello-world/{{ cookiecutter.project_slug }}/src/main/java/hu/sjuhasz/demo/helloworld/App.java
@@ -0,0 +1,11 @@
+package hu.sjuhasz.demo.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
